a07342e5c3dbbaaf538c5bdcca364c244b3651a1
luckeciano <luckeciano@hotmail.com>
2016-09-23 03:26:01 +0000

    fimmm

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 
 vector<char> alphabet;
-int L = 1000000000;
+int L = 100;
 
 bool inAlphabet(char atual) {
     for (int i = 0; i < alphabet.size(); i++) {
@@ -38,42 +38,42 @@ void readAlphabet(string bookPath){
             if (!inAlphabet(atual)) {
                 alphabet.push_back(atual);
             }
-            
+
         }
         myfile.close();
     }
- 
+
 }
 
-string inBin(string Sc, vector<string> dictionary){
-    if (!inDic(Sc, dictionary)){
-        return "";
-    }
-    int pos;
-    int exponent = 0;
-    int noOfBits = 1;
-    for (pos = 0; dictionary[pos] != Sc ; pos++)
-    while(noOfBits < dictionary.size()){
-        noOfBits *= 2;
-        exponent++;
-    }
-    vector<int> ret;
-    while(pos) {
-        if (pos&1)
-            ret.push_back(1);
-        else
-            ret.push_back(0);
-        pos>>=1;  
-    }
-    reverse(ret.begin(),ret.end());
-    stringstream result;
-    copy(ret.begin(), ret.end(), ostream_iterator<int>(result, ""));
-    string binary = result.str().c_str();
-    while (binary.size() < exponent){
-        binary = "0"+binary;
-    }
-    return binary;
+int computeB(int number) {
+    int b;
+    float bFloat = log2(number - 1);
+
+    if (bFloat - long(bFloat) > 0.000000) {
+        b = long(bFloat) + 1;
+    }   else b = long(bFloat);
+    return b;
 }
+
+string inBin (string Sc, vector<string> dictionary) {
+    int j;
+    for (int k  = 0 ; k < dictionary.size(); k++){
+        if (dictionary[k] == Sc) {
+            j = k;
+            break;
+        }
+    }
+    int number = dictionary.size();
+    int b = computeB(number);
+    string r;
+    while(j!=0) {r=(j%2==0 ?"0":"1")+r; j/=2;}
+    while (r.size() < b) {
+        r = "0" + r;
+    }
+    return r;
+
+}
+
 
 
 void init(vector<string> &dictionary) {
@@ -81,52 +81,164 @@ void init(vector<string> &dictionary) {
     for (int i = 0; i < alphabet.size(); i++) {
         dictionary.push_back(string(1, alphabet[i]));
     }
-    
+
 }
 
 void lempelZiv(string bookPath) {
-    
+
     ifstream myfile;
     ofstream compacOutput;
-    compacOutput.open ("output.txt");
+    compacOutput.open ("compressed_data.txt");
     vector<string> dictionary;
-    cout << dictionary.size() << endl;
     init(dictionary);
-    cout << dictionary.size();
     vector<char> Sc;
-    
+
     char atual;
     myfile.open (bookPath);
+    for (int i = 0; i < dictionary.size(); i++) {
+        compacOutput << dictionary[i];
+    }
+    compacOutput << '\\';
     if (myfile.is_open()){
         string Sc = "";
         string S = "";
         while (myfile.get(atual)){
+            /*cout << endl;
+            for (int i = 0; i < dictionary.size(); i++) {
+                cout << dictionary[i] << " ";
+            }
+            cout << endl;*/
             if (dictionary.size() == L) {
                 dictionary.clear();
                 init(dictionary);
+                Sc = "";
             }
+            
             Sc = S + atual;
             if (!inDic(Sc, dictionary)) {
                 dictionary.push_back(Sc);
                 compacOutput << inBin(S, dictionary);
-                //compacOutput << S << endl;
+                //compacOutput << "(" << inBin(S,dictionary) << ")";
+                //compacOutput << S;
+                //cout << dictionary.size() << ":" <<inBin(S, dictionary) << " " << S <<endl;
                 Sc = "";
                 S = atual;
             } else  S += atual;
             
-           
+
+
         }
+        compacOutput << '\\';
+        compacOutput << S;
         myfile.close();
         compacOutput.close();
     }
+
+
+}
+int binToDec(vector<int> binary) {
+    int value= 0;
+    for (int i = binary.size() - 1; i >=0 ; i--) {
+        value += binary[i] * pow(2, binary.size() - 1 - i);
+    }
+
+    return value;
+
+}
+
+void uncompress(string compactedPath) {
+    ifstream myfile;
+    ofstream uncompressedOutput;
+    uncompressedOutput.open("uncompressed_data.txt");
+    vector<string> dictionary;
+    char symbol;
+    dictionary.push_back("");
+
+    myfile.open(compactedPath);
+    myfile.get(symbol);
+    while (symbol != '\\') {
+        string aux = string(1,symbol);
+        dictionary.push_back(aux);
+        myfile.get(symbol);
+    }
+    vector<string> initialDic;
+    initialDic = dictionary;
+
+  
+    if (myfile.is_open()) {
+        int i = dictionary.size() + 1;
+        string S = "";
+        while(!myfile.eof()) {
+            //uncompressedOutput << endl;
+           /* for (int i = 0; i < dictionary.size(); i++) {
+                
+                uncompressedOutput << dictionary[i] << " ";
+            }
+            uncompressedOutput << endl;*/
+             if (i == L + 1) {
+                dictionary.clear();
+                dictionary = initialDic;
+                i = dictionary.size() + 1;
+               // uncompressedOutput << endl;
+                S = "";
+            }
+            if (myfile.peek() == '\\')
+                break;   
+            int b = computeB(i);
+            vector<int> eBin;
+            int t = 0;
+            char auxChar;
+            for (int t = 0; t < b; t++) {
+                char auxChar;
+                myfile.get(auxChar);
+                if (!myfile.eof()) {
+                    int auxInt = auxChar - 48;
+                    eBin.push_back(auxInt);
+                }
+            }
+           /* uncompressedOutput << i << ":";
+            uncompressedOutput << "(";
+            for (int i = 0; i < eBin.size(); i++) {
+                uncompressedOutput << eBin[i];
+            }
+            uncompressedOutput << ")";*/
+            int e = binToDec(eBin);
+            if (e >= dictionary.size()) {
+                S += S[0];
+                uncompressedOutput << S;
+                dictionary.push_back(S);
+            } else {
+                uncompressedOutput << dictionary[e];
+                bool putInDic = false;
+                for (int x = 0; x < dictionary[e].size(); x++) {
+                    S +=  dictionary[e][x];
+                    if (!inDic(S, dictionary)) {
+                        dictionary.push_back(S);
+                        putInDic = true;
+                        S = string (dictionary[e].begin() + x, dictionary[e].end());
+                        break;
+                    }
     
-    
+                }
+            }
+            i ++;
+          
+           
+        }
+        char aux;
+        myfile.get(aux); //simbolo de termino
+        string endString;
+        myfile >> endString;
+        uncompressedOutput << endString;
+
+    }
+
+
 }
 
 int main() {
-    
     readAlphabet("test.txt");
-
     lempelZiv("test.txt");
-    
+    uncompress("compressed_data.txt");
+
 }
